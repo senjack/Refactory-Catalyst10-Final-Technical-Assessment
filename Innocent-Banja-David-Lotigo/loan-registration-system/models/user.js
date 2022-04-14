1082070486ea0e693718f1a1b2204c518317ef1b
kali <innocdavid@gmail.com>
2022-04-14 13:56:29 -0400

    ADDED FEATURE UPDATES -adding register.js -adding user.js -adding regitrations-form.pug -updating app.js

--- Innocent-Banja-David-Lotigo/loan-registration-system/models/user.js
+++ Innocent-Banja-David-Lotigo/loan-registration-system/models/user.js
@@ -4,17 +4,16 @@ const mongoose = require('mongoose')
 //defining the schema
 let tireSchema = mongoose.Schema({
     //fields to insert
-    surname         :{ type:String, required: true },
+    surname         :{ type: String, required: true },
     givenname       :{ type: String, required: true },
-    telephone       :{ type: String, required: true },
-    platenumber     :{ type: String, required: true },
-    carmodel        :{ type: String, required: true },
-    checkintime     :{ type: Date, required: true },
-    tirefault       :{ type: String, required: true},
-    repairstatus    :{ type: String, required: false },
-    cost            :{ type: Number, required: true },
-    mechanic        :{ type: String, required: true},
-    checkouttime    :{ type: Date, required: false}
+    nin             :{ type: Number, required: true },
+    date            :{ type: Date, required: true },
+    occupation      :{ type: String, required: true },
+    work            :{ type: String, required: true},
+    gender          :{ type: String, required: false },
+    loan            :{ type: Number, required: true },
+    security        :{ type: String, required: true},
+    fullback        :{ type: String, required: false}
 })
 
 //exporting the module
--- Innocent-Banja-David-Lotigo/loan-registration-system/routes/register.js
+++ Innocent-Banja-David-Lotigo/loan-registration-system/routes/register.js
@@ -1,11 +1,49 @@
 const express = require('express')
 const router = express.Router()
 
+//importing the model
+let Users = require('../models/user')
 
 //setting the route
 router.get('/', (req, res) => {
     res.render('registration-form')
 })
+
+//adding SUBMIT POST   
+router.post('/', (req, res) => {
+    
+    insertCustomer(req, res)
+})
+
+function insertCustomer(req, res) {
+    //getting the model
+    let user = new Users()
+    //preparing fields to insert to the datebase
+    user.surname        = req.body.surname
+    user.givenname      = req.body.givenname
+    user.nin            = req.body.nin
+    user.date           = req.body.date
+    user.occupation     = req.body.occupation
+    user.work           = req.body.work
+    user.gender         = req.body.gender
+    user.loan           = req.body.loan
+    user.security       = req.body.security
+    user.fullback       = req.body.fullback
+
+
+    //saving the date
+    user.save((err) => {
+        //checking for errors
+        if(err){
+            console.log(err)
+            return
+        } else {
+            
+            res.redirect('/')
+        }
+    })
+}
+
 
 //exporting the route
 module.exports = router
