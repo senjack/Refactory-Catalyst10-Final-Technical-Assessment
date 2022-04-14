fd85427dde090d128e20cdfd7f65ad31ed6535e7
Iradukunda Sam <kunda.sammutunzi@gmail.com>
2022-04-14 16:01:08 +0300

    Iradukunda Sam

---
+++ Iradukunda Sam/Uwezo-project/config/database.js
@@ -0,0 +1,8 @@
+
+//where we locate our database
+module.exports = {
+    // establishing the portnumber to our database
+    
+    database:'mongodb://localhost:27017/Uweza',
+    secret:'secret'
+}
